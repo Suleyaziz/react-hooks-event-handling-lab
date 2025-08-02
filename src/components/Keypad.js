ec2fbeaa4fc688ffca7cf72fa16400ce834a716a
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-08-02 14:05:42 +0300

    Events added

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input 
+            onChange={()=>{
+                return console.log('Entering password...')
+            }} 
+            placeholder="Enter Password Here" 
+            type="password">
+            </input>
+        </div>
     )
 }
 
